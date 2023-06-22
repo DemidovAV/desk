8c0074d8d7954d5fe36c8e35369f0364a5de4e15
DemidovAV <demidovavspb@gmail.com>
2023-06-22 17:02:55 +0300

    git commit

--- src/main/java/com/demoDesk/desk/services/DepartmentService.java
+++ src/main/java/com/demoDesk/desk/services/DepartmentService.java
@@ -1,8 +1,16 @@
 package com.demoDesk.desk.services;
 
+import com.demoDesk.desk.models.Department;
+import com.demoDesk.desk.models.Employee;
 import com.demoDesk.desk.repositories.DepartmentRepository;
+import com.demoDesk.desk.repositories.specifications.DepartmentSpec;
+import com.demoDesk.desk.repositories.specifications.EmployeeSpec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class DepartmentService {
@@ -11,5 +19,28 @@ public class DepartmentService {
     @Autowired
     public void setDepartmentRepository(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
+    }
+
+    public List<Department> getDepartmentsWithFiltering(Specification<Department> specification) {
+        return departmentRepository.findAll(specification);
+    }
+
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Department getDepartmentById(Long id) {
+        return departmentRepository.findOne(DepartmentSpec.findById(id)).orElse(null);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        departmentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void saveElement(Department department) {
+        departmentRepository.save(department);
     }
 }
--- src/main/java/com/demoDesk/desk/services/EmployeeService.java
+++ src/main/java/com/demoDesk/desk/services/EmployeeService.java
@@ -1,8 +1,16 @@
 package com.demoDesk.desk.services;
 
+import com.demoDesk.desk.models.Element;
+import com.demoDesk.desk.models.Employee;
 import com.demoDesk.desk.repositories.EmployeeRepository;
+import com.demoDesk.desk.repositories.specifications.ElementSpec;
+import com.demoDesk.desk.repositories.specifications.EmployeeSpec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
@@ -11,5 +19,28 @@ public class EmployeeService {
     @Autowired
     public void setEmployeeRepository(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getEmployeesWithFiltering(Specification<Employee> specification) {
+        return employeeRepository.findAll(specification);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findOne(EmployeeSpec.findById(id)).orElse(null);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void saveElement(Employee employee) {
+        employeeRepository.save(employee);
     }
 }
