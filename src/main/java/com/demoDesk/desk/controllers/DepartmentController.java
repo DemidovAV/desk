a900410147d9a7192d586e76dca6ffef714ddc48
DemidovAV <demidovavspb@gmail.com>
2023-06-28 21:07:12 +0300

    git commit

--- src/main/java/com/demoDesk/desk/controllers/DepartmentController.java
+++ src/main/java/com/demoDesk/desk/controllers/DepartmentController.java
@@ -1,15 +1,77 @@
 package com.demoDesk.desk.controllers;
 
+import com.demoDesk.desk.dto.elementDto.ShowElements;
+import com.demoDesk.desk.models.nomenclature.Element;
+import com.demoDesk.desk.models.personel.Department;
+import com.demoDesk.desk.repositories.specifications.ElementSpec;
 import com.demoDesk.desk.services.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/departments")
 public class DepartmentController {
     private DepartmentService departmentService;
 
     @Autowired
     public void setDepartmentService(DepartmentService departmentService) {
         this.departmentService = departmentService;
+    }
+
+    private Specification<Element> filtration(String filter, String art) {
+        Specification<Element> spec = Specification.where(null);
+
+        if(filter != null) {
+            spec = spec.and(ElementSpec.titleContains(filter));
+        }
+        if (art != null) {
+            spec = spec.and(ElementSpec.artContains(art));
+        }
+        return spec;
+
+    }
+
+    @GetMapping
+    public List<Department> showDepartments() {
+        return departmentService.getAllDepartments();
+    }
+
+    @GetMapping("/editDepartment/{id}")
+    public Department showEditDepartment(@PathVariable(value="id") Long id){
+        return departmentService.getDepartmentById(id);
+    }
+
+    @PostMapping("/editDepartment/confirm")
+    public boolean editElementConfirm(@ModelAttribute(value="department") Department department) {
+        departmentService.saveElement(department);
+        return true;
+    }
+
+
+    @GetMapping("/addDepartment")
+    public Department addNewElement() {
+        return new Department();
+    }
+    //
+    @PostMapping("/addDepartment/confirm")
+    public boolean addDepartmentConfirm(@ModelAttribute(value="department") Department department) {
+        departmentService.saveElement(department);
+        return true;
+    }
+
+    @GetMapping("/showDepartment/{id}")
+    public Department showOneDepartment(@PathVariable(value="id") Long id) {
+        return departmentService.getDepartmentById(id);
+    }
+
+    @GetMapping("/deleteDepartment/{id}")
+    public boolean deleteDepartment(@PathVariable(value="id") Long id) {
+        departmentService.deleteById(id);
+        return true;
     }
 }
--- src/main/java/com/demoDesk/desk/services/EmployeeService.java
+++ src/main/java/com/demoDesk/desk/services/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.demoDesk.desk.services;
 
 import com.demoDesk.desk.models.personel.Employee;
+import com.demoDesk.desk.repositories.DepartmentRepository;
 import com.demoDesk.desk.repositories.EmployeeRepository;
 import com.demoDesk.desk.repositories.specifications.EmployeeSpec;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,15 @@ import java.util.List;
 public class EmployeeService {
     private EmployeeRepository employeeRepository;
 
+    private DepartmentRepository departmentRepository;
+
     @Autowired
     public void setEmployeeRepository(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+    @Autowired
+    public void setDepartmentRepository(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
     }
 
     public List<Employee> getEmployeesWithFiltering(Specification<Employee> specification) {
