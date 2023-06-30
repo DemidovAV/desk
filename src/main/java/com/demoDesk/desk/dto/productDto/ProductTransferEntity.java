49f3900786ddff04e62d0f0cb1662d461a0d48a3
DemidovAV <demidovavspb@gmail.com>
2023-06-30 17:00:24 +0300

    git commit

---
+++ src/main/java/com/demoDesk/desk/dto/productDto/ProductTransferEntity.java
@@ -0,0 +1,17 @@
+package com.demoDesk.desk.dto.productDto;
+
+
+import java.util.List;
+
+public class ProductTransferEntity {
+
+    private Long id;
+
+    private String art;
+
+    private String title;
+
+    private String description;
+
+    private List<ProductElementInfo> productElementInfoList;
+}
